359a2800b2189fbcb79254de938756a19822d045
SMeijer09 <s.r.meijer.1@student.rug.nl>
2026-04-14 16:53:34 +0200

    update

--- Scripts/FirstModels.R
+++ Scripts/FirstModels.R
@@ -4,5 +4,50 @@ library(lmerTest)
 library(patchwork)
 library(car)
 library(emmeans)
-
 #First models
+
+data <- read.csv("/Users/semmeijer/Downloads/Ecology&Conservation/Flycatcher_Hybridization/Data/database_preferences.csv") |>
+  mutate(patch_h = as.numeric(patch_h),
+         patch_b = as.numeric(patch_b),
+         tail = as.numeric(tail),
+         tarsus = as.numeric(tarsus),
+         wing = as.numeric(wing),
+         mass = as.numeric(mass),
+         beak = as.numeric(beak),
+         patch_size = patch_h*patch_b) |>
+  filter(species == "PF" | species == "CF")
+
+bad_birds <- data |>
+  group_by(ring_nb) |>
+  summarise(n_sexes = n_distinct(sex)) |>
+  filter(n_sexes > 1) |>
+  pull(ring_nb)
+bad_nests <- data |>
+  filter(ring_nb %in% bad_birds) |>
+  pull(yearAreaBox) |>
+  unique()
+bad_nests
+
+data_clean <- data |>
+  filter(!yearAreaBox %in% bad_nests) |>
+  filter(nestbox != "NANA") |>
+  group_by(yearAreaBox) |>
+  mutate(hybridnest = ifelse(n_distinct(species) > 1, 1, 0)) |>
+  mutate(n_birds=n())|>
+  ungroup() |> 
+  mutate(
+    tarsus = ifelse(tarsus <=2 | tarsus >= 50, NA, tarsus),
+    tail   = ifelse(tail >= 100, NA, tail),
+    beak   = ifelse(beak >= 22, NA, beak),
+    mass   = ifelse(mass <= 4 | mass >= 80, NA, mass),
+    wing   = ifelse(wing <= 8, NA, wing),
+    sum_of_white_on_primaries = ifelse(sum_of_white_on_primaries >= 200, NA, sum_of_white_on_primaries),
+    patch_size = ifelse(patch_size >= 300, NA, patch_size)) 
+view(data_clean)
+
+filtered_data <- data_clean |>
+  filter(n_birds <= 2 & n_birds >= 1) |> #filter for all female pied flycatchers and their mate
+  
+
+view(filtered_data)  
+  
