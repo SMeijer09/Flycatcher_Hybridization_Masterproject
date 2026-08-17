ed733d63d9871ca6bf9e715fbc827c134a227a54
SMeijer09 <s.r.meijer.1@student.rug.nl>
2026-08-17 17:38:19 +0200

    update

--- Scripts/Hybridization_timing.R
+++ Scripts/Hybridization_timing.R
@@ -71,10 +71,63 @@ view(combined_data)
 
 #plot laying date and hatching date
 hist(combined_data$laying_date, breaks = 20)
-#plot the mean laying date per year
-combined_data |> group_by(year)|>
-  summarise(meanlaying_date=mean(laying_date)
 
+#compare the mean to the median date
+combined_data |> 
+  group_by(year) |>
+  summarise(mean_laying_date = mean(laying_date, na.rm = TRUE),
+            median_laying_date = median(laying_date, na.rm = TRUE)) |>
+  ggplot(aes(x=year)) +
+  geom_point(aes(y=mean_laying_date), color="blue") +
+  geom_point(aes(y=median_laying_date), color="red") +
+  geom_line(aes(y=mean_laying_date), color="blue") +
+  geom_line(aes(y=median_laying_date), color="red") +
+  theme_classic() +
+  labs(x="Year", y="Laying date") +
+  scale_y_continuous(breaks = seq(0, 200, by = 1)) +
+  scale_x_continuous(breaks = seq(2000, 2023, by = 1)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
+model_data <- combined_data |> select(yearAreaBox, year, nestbox, ring_nb_f, species_f, hybridnest, 
+    laying_date) |> group_by(year) |>
+  mutate(laying_date_relative = laying_date - mean(laying_date, na.rm = TRUE),
+         laying_date_z = (laying_date - mean(laying_date, na.rm = TRUE)) /
+           sd(laying_date, na.rm = TRUE)) |>
+  ungroup()
+view(model_data)
 
+#plot and compare the relative and z laying dates distributions
+ggplot(model_data, aes(x=laying_date_relative)) +
+  geom_histogram(bins=20, fill="blue", alpha=0.5) +
+  theme_classic() +
+  labs(x="Relative laying date", y="Count") +
+  scale_x_continuous(breaks = seq(-50, 50, by = 5)) +
+  scale_y_continuous(breaks = seq(0, 100, by = 10))
+ggplot(model_data, aes(x=laying_date_z)) +
+  geom_histogram(bins=20, fill="red", alpha=0.5) +
+  theme_classic() +
+  labs(x="Z laying date", y="Count") +
+  scale_x_continuous(breaks = seq(-3, 3, by = 0.5)) +
+  scale_y_continuous(breaks = seq(0, 100, by = 10))
+
+#now model the effect of laying date on hybridnest
+m1 <- glmer(hybridnest ~ laying_date_relative + (1|year), data=model_data, family=binomial)
+summary(m1)
+m2 <- glmer(hybridnest ~ laying_date_relative * species_f + (1|year), data=model_data, family=binomial)
+summary(m2)
+m3 <- glmer(hybridnest ~ laying_date_z + (1|year), data=model_data, family=binomial)
+summary(m3)
+m4 <- glmer(hybridnest ~ laying_date_z * species_f + (1|year), data=model_data, family=binomial)
+summary(m4)
+
+#plot the effect of laying date on hybridnest per species
+ggplot(model_data, aes(x=laying_date_relative, y=hybridnest, color=species_f)) +
+  geom_point(alpha=0.5) +
+  geom_smooth(method="glm", method.args=list(family="binomial"), se=TRUE) +
+  theme_classic() +
+  labs(x="Relative laying date", y="Probability of hybrid nest") +
+  scale_x_continuous(breaks = seq(-50, 50, by = 5)) +
+  scale_y_continuous(breaks = seq(0, 1, by = 0.1))
+
+#ok this is for the laying date adjusted to the two species together, but they have different timing so maybe it should be seperated instead!
 
